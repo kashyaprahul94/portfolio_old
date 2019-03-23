c1fb0982fcd1e238e902065f77b08a7b3f97a31d
Rahul Kashyap <kashyaprahul94@gmail.com>
2019-03-23 10:46:25 +0100

    compacted the base

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import Home from "./pages/home";
+
+export default class extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Home />
+      </div>
+    );
+  }
+}
